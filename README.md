14100e54bee8d57a034bc39be1d55a9ece2ebc17
WMYeah_Mac <george012@163.com>
2023-07-20 21:25:44 +0800

    Update v1.0.1

--- README.md
+++ README.md
@@ -1,41 +1,53 @@
-# r2-release action
+<!-- TOC -->
 
-This action uploads a file to Cloudflare R2 (or any other S3 provider)
+- [1. cloudflare-r2-upload action script](#1-cloudflare-r2-upload-action-script)
+    - [1.1. Example usage](#11-example-usage)
+    - [1.2. Inputs](#12-inputs)
+        - [1.2.1. `endpoint`](#121-endpoint)
+        - [1.2.2. `accesskeyid`](#122-accesskeyid)
+        - [1.2.3. `secretaccesskey`](#123-secretaccesskey)
+        - [1.2.4. `bucket`](#124-bucket)
+        - [1.2.5. `file`](#125-file)
+        - [1.2.6. `destination`](#126-destination)
 
-## Inputs
+<!-- /TOC -->
 
-## `endpoint`
+# 1. cloudflare-r2-upload action script
+* This action uploads a file to Cloudflare R2 (or any other S3 provider)
 
-**Required** The S3 endpoint URL.
-
-## `accesskeyid`
-
-**Required** The S3 access key ID.
-
-## `secretaccesskey`
-
-**Required** The S3 access key.
-
-## `bucket`
-DEAR GOD<br>
-**Required** The S3 bucket you want to upload to.
-
-## `file`
-
-**Required** Which file you want to upload
-
-## `destination`
-
-**Optional** Where you want the file to end up. Defaults to '/(filename)'.
-
-## Example usage
-```yaml
-uses: randomairborne/r2-release@main
+## 1.1. Example usage
+```
+uses: george012/cloudflare-r2-upload@latest
 with:
-  endpoint: ${{ secrets.S3_ENDPOINT }}
-  accesskeyid: ${{ secrets.S3_ACCESS_KEY_ID }}
-  secretaccesskey: ${{ secrets.S3_SECRET_ACCESS_KEY }}
-  bucket: 'my-s3-bucket'
-  file: './target/release/my-app.exe'
-  destination: '/download/my-app.exe'
-  ```
+  endpoint: ${{ secrets.S3_COMPATIBILITY_ENDPOINT }}
+  accesskeyid: ${{ secrets.S3_COMPATIBILITY_ACCESS_KEY_ID }}
+  secretaccesskey: ${{ secrets.S3_COMPATIBILITY_SECRET_ACCESS_KEY }}
+  bucket: 'bucket_name'
+  file: './custom_path/pre_upload_file_pre.zip'
+  destination: '/public_path/upload_over_file_name.zip'
+```
+
+## 1.2. Inputs
+
+### 1.2.1. `endpoint`
+
+* <font color=red>Required</font> The S3 endpoint URL.
+
+### 1.2.2. `accesskeyid`
+
+* <font color=red>Required</font> The S3 access key ID.
+
+### 1.2.3. `secretaccesskey`
+
+* <font color=red>Required</font> The S3 access key.
+
+### 1.2.4. `bucket`
+* <font color=red>Required</font>  The S3 bucket you want to upload to.
+
+### 1.2.5. `file`
+
+* <font color=red>Required</font> Which file you want to upload
+
+### 1.2.6. `destination`
+
+* <font color=green>Optional</font> Where you want the file to end up. Defaults to '/(filename)'.
---
+++ git_push.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+set -e
+
+ProductName=cloudflare-r2-upload
+REPO_PFEX=george012/$ProductName
+VersionFile=./package.json
+
+CurrentVersionString=$(jq -r '.version' $VersionFile)
+
+versionStr=""
+
+function to_run() {
+    if [ -z "$1" ]; then
+        echo "============================ ${ProductName} ============================"
+        echo "  1、发布 [-${ProductName}-]"
+        echo "  当前版本[-${CurrentVersionString}-]"
+        echo "======================================================================"
+        read -p "$(echo -e "请输入版本号[例如；v0.0.1]")" inputString
+        if [[ "$inputString" =~ ^v.* ]]; then
+            versionStr=${inputString}
+        else
+            versionStr=v${inputString}
+        fi
+        return 0
+    elif [ "$1" == "auto" ] || [ "$1" == "" ]; then
+        baseStr=$(echo $CurrentVersionString | cut -d'.' -f1)     # Get the base version (v0)
+        base=${baseStr//v/}                                       # Get the base version (0)
+        major=$(echo $CurrentVersionString | cut -d'.' -f2)       # Get the major version (0)
+        minor=$(echo $CurrentVersionString | cut -d'.' -f3)       # Get the minor version (1)
+
+        minor=$((minor+1))                          # Increment the minor version
+        if ((minor==100)); then                     # Check if minor version is 100
+            minor=0                                 # Reset minor version to 0
+            major=$((major+1))                      # Increment major version
+        fi
+
+        if ((major==100)); then                     # Check if major version is 100
+            major=0                                 # Reset major version to 0
+            base=$((base+1))                        # Increment base version
+        fi
+
+        versionStr="v${base}.${major}.${minor}"
+        return 0
+    else
+      return 1
+    fi
+}
+
+function get_pre_version_no {
+    local v_str=$1
+    baseStr=$(echo $v_str | cut -d'.' -f1)     # Get the base version (v0)
+    base=${baseStr//v/}                                       # Get the base version (0)
+    major=$(echo $v_str | cut -d'.' -f2)       # Get the major version (0)
+    minor=$(echo $v_str | cut -d'.' -f3)       # Get the minor version (1)
+
+    if ((minor>0)); then                      # Check if minor version is more than 0
+        minor=$((minor-1))                     # Decrement the minor version
+    else
+        minor=99                               # Reset minor version to 99
+        if ((major>0)); then                   # Check if major version is more than 0
+            major=$((major-1))                 # Decrement major version
+        else
+            major=99                           # Reset major version to 99
+            if ((base>0)); then                # Check if base version is more than 0
+                base=$((base-1))               # Decrement base version
+            else
+                echo "Error: Version cannot be decremented."
+                exit 1
+            fi
+        fi
+    fi
+
+    pre_v_no="${base}.${major}.${minor}"
+    echo $pre_v_no
+}
+
+function git_handle() {
+    current_version_no=${CurrentVersionString//v/}
+    netx_version_no=${versionStr//v/}
+
+    jq ".version = \"${netx_version_no}\"" $VersionFile > temp.json && mv temp.json $VersionFile
+
+    pre_del_version_no=$(get_pre_version_no "$current_version_no")
+
+    git add . \
+    && git commit -m "Update v${netx_version_no}"  \
+    && git tag v$netx_version_no \
+    && git push \
+    && git push --tags \
+    && git tag -f latest $netx_version_no \
+    && git push -f origin latest \
+    && git tag -d v$pre_del_version_no
+}
+
+
+if to_run "$1"; then
+    git_handle
+
+    echo "Complated"
+else
+    echo "Invalid argument"
+fi
+
+
